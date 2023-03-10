0b1591e27593c4b1d521a2fab19b6d412b246758
arriusz97 <65055189+arriusz97@users.noreply.github.com>
2023-03-10 15:01:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import numpy as np
 
 st.title('전세계 코로나 데이터터 분석')
 
-coronaDf = pd.read_csv('covid_19_clean_complete')
+coronaDf = pd.read_csv('covid_19_clean_complete.csv')
 
 st.dataframe(data = coronaDf)
